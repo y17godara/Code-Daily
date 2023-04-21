a0cdab918d8e97b43e22af6b6a0d0f43e06c8ad3
Yash Godara <yash17godara@gmail.com>
2023-04-21 11:51:41 +0530

    changes

---
+++ Code/nugget.js
@@ -0,0 +1,14 @@
+const people = [
+  {
+    name : "example_name",
+    age : 21,
+    country : "example_country",
+  },
+  {
+    name : "sussy",
+    age : 18,
+    country : "england",
+  },
+];
+
+console.log(ages);
