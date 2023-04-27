5cdd26eb70afa810d37eea75e76d438b50320912
Yash Godara <yash17godara@gmail.com>
2023-04-27 08:48:07 +0530

    PORT with LOGIN FORM
    setup and done

--- Node/app.js
+++ Node/app.js
@@ -1,28 +1,45 @@
-//Learning Middleware
-// req => middleware => res
-const express = require('express');
-const app = express();
-const logger = require('./middleware.js');
-const auth = require('./autherize.js');
+// // read data
+// const express = require('express');
+// const app = express();
+// const { people } = require('./4-data.js');
 
-// https://www.npmjs.com/package/morgan //for logger aleternative
-app.use('/', [logger,])
-app.use('/api', auth) //auth for api
 
-app.get('/', (req, res) => {
-    res.send('Home Page');
-});
+// //static data
+// app.use(express.static('./public'))
 
-app.get('/about', (req, res) => {
-    res.send('About Page');
+// //api
+// app.get('/api/people', (req, res) => {
+//     res.status(200).json({ success: true, data: people});
+// })
+
+// //listen
+// app.listen(5000, () => {
+//     console.log('Server Started');
+//})
+
+const express = require('express')
+const app = express()
+let { people } = require('./data.js')
+
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/api/people', (req,res) => {
+    res.status(200).json({success: true, data: people});
 })
 
-//auth
-app.get('/api/v1', (req, res) => {
-    console.log(req.user); //console log the user details
-    res.send('API Page');
+app.post('/login', (req,res) => {
+  const { name } = req.body;
+  if(name) {
+    console.log(`User ${name} Loggined`);
+    return res.status(200).send(`Welcome "${name}" `);
+  }
+  console.log(`User Loggined Fails`);
+  res.status(401).send(`Please Provide Credentials`);
 })
 
 app.listen(5000, () => {
-    console.log('Server Started');
+  console.log('Server is listening on port 5000....')
 })
---
+++ Node/data.js
@@ -0,0 +1,10 @@
+const people = [
+    { id: 1, name: "john" },
+    { id: 2, name: "peter" },
+    { id: 3, name: "susan" },
+    { id: 4, name: "anna" },
+    { id: 5, name: "emma" },
+  ];
+
+  // exports
+module.exports = { people };
